64b5c40ed339a2f99a568afe651ab2753a007cae
Shantanu Goyal <shantanugoyal23@gmail.com>
2025-09-21 12:47:18 +0530

    chore: add webpack as bundler

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+import path from 'path';
+
+export default {
+  target: "node", // VS Code extensions run in Node
+  entry: "./src/extension.ts", // entry point of your extension
+  output: {
+    path: path.resolve(process.cwd(), "dist"),
+    filename: "extension.js",
+    libraryTarget: "commonjs2",
+  },
+  devtool: "source-map",
+  externals: {
+    vscode: "commonjs vscode", // don't bundle VS Code's own API
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: "ts-loader",
+      },
+    ],
+  },
+};
